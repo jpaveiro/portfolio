1520aabc41f0e74777ac09f7a6633353de53b8d4
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-05-08 00:30:04 -0300

    feat: redirect to projects

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor() {}
 
+  redirectTo(path: string) {
+    location.href = path;
+  }
 }
