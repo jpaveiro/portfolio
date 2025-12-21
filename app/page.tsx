9efbdaf75eba93d1a2cf1079c8f0179e66e9d463
jpaveiro <jpaveiros@gmail.com>
2025-12-20 23:27:48 -0300

    feat: add "Conquistas" on navbar options

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Feats from "@/components/feats";
+import { GithubIcon, LinkedinIcon } from "@/components/ui/icon";
 import {
   Mail,
   Terminal,
@@ -11,40 +12,6 @@ import {
   Layers,
   FileUserIcon,
 } from "lucide-react";
-
-/* TODO: Componentização */
-
-const GithubIcon = ({ size = 24, className = "" }) => (
-  <svg
-    role="img"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    fill="currentColor"
-    className={className}
-  >
-    <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.604-2.665-.305-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-  </svg>
-);
-
-const LinkedinIcon = ({ size = 24, className = "" }) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className={className}
-  >
-    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-    <rect width="4" height="12" x="2" y="9" />
-    <circle cx="4" cy="4" r="2" />
-  </svg>
-);
 
 export default function Home() {
   const iam = {
@@ -134,16 +101,18 @@ export default function Home() {
             <Terminal size={20} className="text-purple-500" /> {iam.name}
           </h1>
           <ul className="flex gap-6 text-sm font-medium text-gray-400">
-            {["Início", "Tecnologias", "Conquistas", "Projetos", "Contato"].map((item) => (
-              <li key={item}>
-                <a
-                  href={`#${item.toLowerCase()}`}
-                  className="hover:text-white hover:underline decoration-purple-500 decoration-2 underline-offset-4 transition-all"
-                >
-                  {item}
-                </a>
-              </li>
-            ))}
+            {["Início", "Tecnologias", "Conquistas", "Projetos", "Contato"].map(
+              (item) => (
+                <li key={item}>
+                  <a
+                    href={`#${item.toLowerCase()}`}
+                    className="hover:text-white hover:underline decoration-purple-500 decoration-2 underline-offset-4 transition-all"
+                  >
+                    {item}
+                  </a>
+                </li>
+              )
+            )}
           </ul>
         </div>
       </nav>
